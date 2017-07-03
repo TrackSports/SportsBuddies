73bf2bb0ecf7676e7f61d73639d4d53f940197d8
INFOTRACK\fernando.charnis <fernando.charnis@infotrack.com.au>
2017-07-03 19:24:33 +1000

    no message

--- server/TrackSports_API/Controllers/TrackSportsController.cs
+++ server/TrackSports_API/Controllers/TrackSportsController.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TrackSports.Authentication;
+using TrackSports_API.TracksportModels;
 
 namespace TrackSports_API.Controllers
 {
@@ -11,9 +14,24 @@ namespace TrackSports_API.Controllers
     [Route("api/TrackSports")]
     public class TrackSportsController : Controller
     {
-        public TrackSportsController()
+
+        [HttpPost()]
+        [AllowAnonymous]
+        public IActionResult GetAllEvents()
         {
-            var a = "ad";
+            try
+            {
+                List<Event> events=new List<Event>();
+                events.Add(new Event(){Id = 1, Name = "Footballl", Category = "Football", EventDay = "Monday", Location = "The Park", TimeStart = "12:00", Duration = 45});
+                events.Add(new Event() { Id = 2, Name = "num 2", Category = "Netball", EventDay = "Tuesday", Location = "123 gim st. CBD", TimeStart = "12:45", Duration = 90 });
+                events.Add(new Event() { Id = 3, Name = "Rugby", Category = "AFL", EventDay = "Saturday", Location = "The Other Park", TimeStart = "19:00", Duration = 120 });
+                return Ok(events);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex);
+            }
         }
+
     }
 }
---
+++ server/TrackSports_API/TracksportModels/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackSports_API.TracksportModels
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Location { get; set; }
+        public string EventDay { get; set; }
+        public string TimeStart { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
