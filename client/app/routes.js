cf05a458f536127a8b2eba6f4b3482475de4980a
Xinxin-InfoTrack <Xinxin.Li@infotrack.com.au>
2017-07-03 11:06:50 +1000

    update the router to hackit

--- client/app/routes.js
+++ client/app/routes.js
@@ -5,7 +5,7 @@ import NSWVS from 'features/nsw/components/VendorSolicitor';
 import NSWPS from 'features/nsw/components/PurchaserSolicitor';
 
 export default (
-  <Route path="/flexit" component={Layout}>
+  <Route path="/hackit" component={Layout}>
     <Route path="nswvs/:documentAccessCode" component={NSWVS} />
     <Route path="nswps/:documentAccessCode" component={NSWPS} />
     <Route path="demo/new" component={NSWVS} />
