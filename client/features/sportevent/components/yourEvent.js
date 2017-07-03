6c3dcbd5092766c58c5cf7a085c8966b8c9f89a6
INFOTRACK\fernando.charnis <fernando.charnis@infotrack.com.au>
2017-07-03 15:23:18 +1000

    no message

--- client/features/sportevent/components/yourEvent.js
+++ client/features/sportevent/components/yourEvent.js
@@ -1,32 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 // import classnames from 'classnames';
-// import { connect } from 'react-redux';
-// import Spinner from 'share/Spinner';
-// import ActionSpinner from 'share/ActionSpinner';
-// import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getEventList } from 'features/sportevent/actions/toolbarAction';
 
-export default class YourEvent extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   componentDidMount() {
-//   }
-
-//   componentWillReceiveProps(nextProps) {
-//   }
+export class Toolbar extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
+  }
+
+  showTheEvent(EventId) {
+    alert(EventId);
+    // this.props.getEventList();
   }
 
   render() {
     return (
       <div>
           <div>Your Events</div>
-          <div>search box component</div>
-          <div>Your event list</div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <form className="navbar-form navbar-left">
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Search" />
+              </div>
+            </form>
+          </div>
+
+          <div>
+            <ul>
+                <li><a onClick={() => this.showTheEvent('1') }>Sat Football</a></li>
+                <li><a onClick={() => this.showTheEvent('2') }>Sun Netball</a></li>
+                <li><a onClick={() => this.showTheEvent('3') }>Wed Yoga</a></li>
+                <li><a onClick={() => this.showTheEvent('4') }>Thur Weights</a></li>
+            </ul>
+          </div>
       </div>
     );
   }
 }
+
+Toolbar.propTypes = {
+  getEventList: PropTypes.func
+};
+
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators({ getEventList }, dispatch);
+}
+
+export default connect(null, matchDispatchToProps)(Toolbar);
