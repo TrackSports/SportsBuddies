2cfe24e1626f74884dd205db36a3ef0d843b3a1a
Xinxin-InfoTrack <Xinxin.Li@infotrack.com.au>
2017-07-04 10:00:16 +1000

    add the ajax load

--- client/features/sportevent/actions/eventDetailAction.js
+++ client/features/sportevent/actions/eventDetailAction.js
@@ -2,3 +2,4 @@ import * as ActionTypes from './actionTypes';
 import createAction from 'utils/createAction';
 
 export const showEventDetail = createAction(ActionTypes.SHOW_EVENT_DETAIL);
+export const getAllRelatedEvent = createAction(ActionTypes.GET_ALL_RELEATED_EVENT);
--- client/features/sportevent/actions/toolbarAction.js
+++ client/features/sportevent/actions/toolbarAction.js
@@ -8,3 +8,5 @@ export const createEvent = createAction(ActionTypes.CREATE_EVENT);
 export const createEventSuccess = createAction(ActionTypes.CREATE_EVENT_SUCCESS);
 export const closeNewEventModal = createAction(ActionTypes.HIDE_NEW_EVENT_MODAL);
 export const joinEvent = createAction(ActionTypes.JOIN_EVENT);
+
+export const getAllEvent = createAction(ActionTypes.GET_ALL_EVENT);
